e48eacd83c0d2a99708a683bf552e1682ce12be7
Gianmaria <gianmaria.bonici@gmail.com>
2023-12-14 19:13:30 +0100

    Day 10 part 2...

--- Day10/src/day10.cpp
+++ Day10/src/day10.cpp
@@ -218,6 +218,8 @@ struct Tile
 
     Direction conn1 {};
     Direction conn2 {};
+
+    bool part_of_loop {false};
 };
 
 using Maze = vec<vec<Tile>>;
@@ -233,10 +235,10 @@ bool operator!=(const Tile& t, char ch)
 }
 
 
-const Tile* can_move(const Maze& maze, const Tile& tile,
-                     Direction dir, bool start_tile = false)
+Tile* can_move(Maze& maze, const Tile& tile,
+               Direction dir, bool start_tile = false)
 {
-    const Tile* next_tile {nullptr};
+    Tile* next_tile {nullptr};
 
     switch (dir)
     {
@@ -266,9 +268,9 @@ const Tile* can_move(const Maze& maze, const Tile& tile,
 
         case Direction::right:
         {
-            if (tile.c + 1 < maze.size()) // assumed maze is always sqare
+            if (tile.c + 1 < maze.at(0).size()) // assumed maze is always sqare
             {
-                next_tile = &(maze.at(tile.r).at(tile.c + 1));
+                next_tile = &maze.at(tile.r).at(tile.c + 1);
             }
         } break;
 
@@ -390,7 +392,7 @@ void part1()
 
                 default:
                 throw std::format("Unknown cell <{} ({},{})>",
-                                  c, r, c);
+                                  ch, r, c);
             }
 
             ++c;
@@ -483,15 +485,229 @@ void part1()
     cout << "day 10 part 1 (" << file_path << ") " << res << endl;
 }
 
+void draw_maze(const Maze& maze)
+{
+    ranges::for_each(maze, [](const auto& row) {
+        ranges::for_each(row, [](const Tile& tile) {
+            if (tile.part_of_loop)
+                cout << tile.name;
+            else if (str("I").contains(tile.name))
+                cout << tile.name;
+            else
+                cout << ".";
+        });
+        cout << endl;
+    });
+}
+
 void part2()
 {
+    //#define SANITY_CHECK
+
     auto file_path = "res\\input.txt";
     auto ifs = std::ifstream(file_path);
     if (not ifs.is_open())
         throw std::format("Cannot open file <{}>", file_path);
 
+    Maze maze;
+    i32 tile_start_r {0};
+    i32 tile_start_c {0};
 
-    auto res = 0;
+    {
+        i32 r = 0;
+        for (str line;
+             std::getline(ifs, line);
+             )
+        {
+            vec<Tile> row;
+            i32 c = 0;
+
+            for (char ch : line)
+            {
+                Tile tile;
+
+                tile.name = ch;
+
+                tile.r = r;
+                tile.c = c;
+
+                switch (ch)
+                {
+                    case '|':
+                    {
+                        tile.conn1 = Direction::up;
+                        tile.conn2 = Direction::down;
+                    } break;
+
+                    case '-':
+                    {
+                        tile.conn1 = Direction::left;
+                        tile.conn2 = Direction::right;
+                    } break;
+
+                    case 'L':
+                    {
+                        tile.conn1 = Direction::up;
+                        tile.conn2 = Direction::right;
+                    } break;
+
+                    case 'J':
+                    {
+                        tile.conn1 = Direction::up;
+                        tile.conn2 = Direction::left;
+                    } break;
+
+                    case '7':
+                    {
+                        tile.conn1 = Direction::down;
+                        tile.conn2 = Direction::left;
+                    } break;
+
+                    case 'F':
+                    {
+                        tile.conn1 = Direction::down;
+                        tile.conn2 = Direction::right;
+                    } break;
+
+                    case '.':
+                    {
+                        tile.conn1 = Direction::none;
+                        tile.conn2 = Direction::none;
+                    } break;
+
+                    case 'S':
+                    {
+                        tile.conn1 = Direction::none;
+                        tile.conn2 = Direction::none;
+
+                        tile_start_r = r;
+                        tile_start_c = c;
+                    } break;
+
+                    default:
+                    throw std::format("Unknown cell <{} ({},{})>",
+                                      ch, r, c);
+                }
+
+                ++c;
+                row.push_back(std::move(tile));
+            }
+
+            ++r;
+
+            maze.push_back(std::move(row));
+        }
+    }
+
+    Tile* start = &maze.at(tile_start_r).at(tile_start_c);
+    start->part_of_loop = true;
+
+    // setup connection for start tile
+    for (Direction dir : {Direction::up, Direction::down, Direction::left, Direction::right})
+    {
+        if (can_move(maze, *start, dir, true))
+        {
+            if (start->conn1 == Direction::none)
+                start->conn1 = dir;
+            else if (start->conn2 == Direction::none)
+                start->conn2 = dir;
+            else
+                throw "??????";
+        }
+    }
+
+    Tile* curr_tile = start;
+    Direction from = Direction::none;
+    do
+    {
+#ifdef SANITY_CHECK
+        i32 movs = 0;
+        cout << "(" << curr_tile->r + 1 << "," << curr_tile->c + 1 << ") tile " << curr_tile->name << " [";
+        for (Direction dir : {Direction::up, Direction::down, Direction::left, Direction::right})
+        {
+            if (dir != from and
+                can_move(maze, *curr_tile, dir))
+            {
+                cout << dir << ", ";
+                ++movs;
+            }
+        }
+        cout << "] (" << movs << ")" << endl;
+
+        if (movs > 1)
+        {
+            int s = 0;
+        }
+
+        bool moved = false;
+#endif // SANITY_CHECK
+
+        for (Direction dir : {Direction::up, Direction::down, Direction::left, Direction::right})
+        {
+            if (dir != from)
+            {
+                if (auto* new_tile = can_move(maze, *curr_tile, dir);
+                    new_tile != nullptr)
+                {
+                    curr_tile = new_tile;
+                    curr_tile->part_of_loop = true;
+                    from = opposite(dir); // keep track of where you come from
+#ifdef SANITY_CHECK
+                    moved = true;
+#endif // SANITY_CHECK
+                    break;
+                }
+            }
+        }
+
+#ifdef SANITY_CHECK
+        if (not moved)
+        {
+            // stuck? 
+            int s = 0;
+            throw "i'm stuck!";
+        }
+#endif // SANITY_CHECK
+
+    } while (curr_tile->name != 'S');
+
+    // calculate Point in polygon
+    u32 inside {0};    
+    for (auto [r, row&] : maze | std::views::enumerate)
+    {
+        for (auto [c, tile&] : row | std::views::enumerate)
+        {
+            // if the point we want to start tracing from
+            // is part of the loop, it's automatically
+            // outside the loop
+            if (tile.part_of_loop)
+                continue;
+            
+            // start scanning from left to right
+            u32 intersection {0};
+            for (u64 cc = c;
+                 cc < maze.at(0).size();
+                 ++cc)
+            {
+                const auto* tile = &maze.at(r).at(cc);
+
+                if (tile->part_of_loop and
+                    str("|LJ").contains(tile->name)) // why 'L' and 'J'? i have no idea
+                {
+                    ++intersection;
+                }
+            }
+
+            if (intersection % 2 == 1)
+            {
+                ++inside;
+            }
+        }
+    }
+
+    //draw_maze(maze);
+
+    auto res = inside;
     cout << "day 10 part 2 (" << file_path << ") " << res << endl;
 }
 
