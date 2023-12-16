b6a93c3fd7ca53cca89d149f98bb65b46f574317
Gianmaria <gianmaria.bonici@gmail.com>
2023-12-16 17:05:46 +0100

    fuck manhattan distance!

--- Day11/src/day11.cpp
+++ Day11/src/day11.cpp
@@ -172,125 +172,92 @@ bool operator==(const Point& a, const Point& b)
     return a.r == b.r and a.c == b.c;
 }
 
-
-Image parse_input(std::ifstream& stream)
+bool is_row_empty(const vec<vec<char>>& map, u64 r)
 {
-    Image image;
-
-    u16 galaxy_counter {0};
-    for (str line;
-         std::getline(stream, line);
-         )
-    {
-        image.push_back({});
-        ranges::for_each(line, [&image, &galaxy_counter](char ch)
-        {
-            if (ch == '.')
-            {
-                image.back().push_back(0);
-            }
-            else
-            {
-                image.back().push_back(++galaxy_counter);
-            }
-        });
-    }
-
-    return image;
-}
-
-void expand_universe(Image& image)
-{
-    for (auto it = image.begin();
-         it != image.end();
-         ++it)
-    {
-        if (std::all_of(it->begin(), it->end(), [](auto elem) { return elem == 0; }))
-        {
-            it = image.insert(it + 1, vec<u16>(it->size(), 0));
-            it += 1; // skip inserted row
-        }
-    }
+    bool empty = true;
 
     for (u64 c = 0;
-         c < image.at(0).size();
+         c < map.at(r).size();
          ++c)
     {
-        auto all_dots = std::all_of(image.begin(), image.end(),
-                                    [c](const auto& row)
+        if (map.at(r).at(c) == '#')
         {
-            return row[c] == 0;
-        });
-
-        if (all_dots)
-        {
-            // Insert a new column with dots at the current position
-            for (auto& row : image)
-            {
-                row.insert(row.begin() + c, 0);
-            }
-            ++c;
+            empty = false;
+            break;
         }
     }
+    return empty;
 }
 
-u32 solve(const Image& image)
+bool is_col_empty(const vec<vec<char>>& map, u64 c)
 {
-    vec<Point> galaxies;
+    bool empty = true;
+
+    for (u64 r = 0;
+         r < map.size();
+         ++r)
+    {
+        if (map.at(r).at(c) == '#')
+        {
+            empty = false;
+            break;
+        }
+    }
+    return empty;
+}
+
+u64 solve(const vec<vec<char>>& map, u64 offset)
+{
+    vec<pair<u64, u64>> galaxies;
 
     // collect all the galaxies
-    for (const auto& [r, row] : std::views::enumerate(image))
+    for (const auto& [r, row] : std::views::enumerate(map))
     {
-        for (const auto& [c, value] : std::views::enumerate(row))
+        for (const auto& [c, ch] : std::views::enumerate(row))
         {
-            if (value != 0)
+            if (ch == '#')
             {
                 galaxies.emplace_back(r, c);
             }
         }
     }
 
-    map<pair<u32, u32>, u32> res;
-
-    for (const Point& galaxy_a : galaxies)
+    offset -= 1;
+    u64 acc = 0;
+    for (auto gal_a = galaxies.begin();
+         gal_a != galaxies.end();
+         ++gal_a)
     {
-        for (const Point& galaxy_b : galaxies)
+        for (auto gal_b = gal_a + 1;
+             gal_b != galaxies.end();
+             ++gal_b)
         {
-            if (galaxy_b == galaxy_a)
-                continue;
+            auto from = std::make_pair(std::min(gal_a->first, gal_b->first), std::min(gal_a->second, gal_b->second));
+            auto to = std::make_pair(std::max(gal_a->first, gal_b->first), std::max(gal_a->second, gal_b->second));
 
-            auto from = image[galaxy_a.r][galaxy_a.c];
-            auto to = image[galaxy_b.r][galaxy_b.c];
-
-            auto from_to = std::make_pair(from, to);
-            auto to_from = std::make_pair(to, from);
-
-            if (
-                (res.find(from_to) != res.end())
-                or
-                (res.find(to_from) != res.end())
-                )
+            for (u64 r = from.first;
+                 r < to.first;
+                 ++r)
             {
-                continue;
+                ++acc;
+
+                if (is_row_empty(map, r))
+                    acc += offset;
             }
 
-            u32 dist = std::abs(galaxy_a.r - galaxy_b.r) + std::abs(galaxy_a.c - galaxy_b.c);
+            for (u64 c = from.second;
+                 c < to.second;
+                 ++c)
+            {
+                ++acc;
 
-            //cout << std::format("from galaxy {} ({},{}) to {} ({},{}) length {}",
-            //                    from, galaxy_a.r + 1, galaxy_a.c + 1,
-            //                    to, galaxy_b.r + 1, galaxy_b.c + 1,
-            //                    dist)
-            //    << endl;
-
-            res.insert({from_to, dist});
+                if (is_col_empty(map, c))
+                    acc += offset;
+            }
         }
-        //cout << endl;
     }
 
-    return std::accumulate(res.begin(), res.end(), 0U,
-                           [](u32 acc, const auto& pair) {
-        return acc + pair.second;
-    });
+    return acc;
 }
 
 void part1()
@@ -300,12 +267,16 @@ void part1()
     if (not ifs.is_open())
         throw std::format("Cannot open file <{}>", file_path);
 
-    Image image = parse_input(ifs);
+    vec<vec<char>> map;
 
-    expand_universe(image);
+    for (str line;
+         std::getline(ifs, line);
+         )
+    {
+        map.push_back(vec<char>(line.begin(), line.end()));
+    }
 
-    auto res = solve(image);
-
+    auto res = solve(map, 2);
     cout << "day 11 part 1 (" << file_path << ") " << res << endl;
 }
 
@@ -316,20 +287,22 @@ void part2()
     if (not ifs.is_open())
         throw std::format("Cannot open file <{}>", file_path);
 
+    vec<vec<char>> map;
+
     for (str line;
          std::getline(ifs, line);
          )
     {
-
+        map.push_back(vec<char>(line.begin(), line.end()));
     }
 
-    auto res = -1;
+    auto res = solve(map, 1000000);
     cout << "day 11 part 2 (" << file_path << ") " << res << endl;
 }
 
 int main()
 {
-    try 
+    try
     {
         part1();
         part2();
