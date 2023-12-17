298518612e6c518508a01f51275f973525039112
Gianmaria <gianmaria.bonici@gmail.com>
2023-12-17 12:01:34 +0100

    Day 15 part 1

---
+++ Day15/src/day15.cpp
@@ -0,0 +1,229 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+#include <array>
+#include <assert.h>
+#include <cstdint>
+#include <exception>
+#include <utility>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <unordered_map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <regex>
+#include <set>
+#include <string>
+#include <string_view>
+#include <vector>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::string_view;
+using std::vector;
+using std::set;
+using std::map;
+using std::pair;
+
+using str = std::string;
+using str_cref = std::string const&;
+
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+namespace fs = std::filesystem;
+namespace ranges = std::ranges;
+
+template<typename T>
+using vec = vector<T>;
+
+
+// ==============================================
+// ==============================================
+//
+//  _    _   _______   _____   _         _____  
+// | |  | | |__   __| |_   _| | |       / ____| 
+// | |  | |    | |      | |   | |      | (___   
+// | |  | |    | |      | |   | |       \___ \  
+// | |__| |    | |     _| |_  | |____   ____) | 
+//  \____/     |_|    |_____| |______| |_____/  
+//                                              
+//                                               
+// ==============================================
+// ==============================================
+
+str read_file(const char* file_path)
+{
+    auto ifs = std::ifstream(file_path);
+
+    if (not ifs.is_open())
+        throw std::runtime_error("[ERROR] Cannot open file "s + file_path);
+
+    //std::stringstream buffer;
+    //buffer << ifs.rdbuf();
+    //return buffer.str();
+
+    return str(std::istreambuf_iterator<char>{ifs}, {});
+}
+
+// Trim from the beginning (left)
+str ltrim(str_cref s)
+{
+    auto it = std::find_if(s.begin(), s.end(), [](char ch)
+    {
+        return !std::isspace(static_cast<unsigned char>(ch));
+    });
+    return str(it, s.end());
+}
+
+// Trim from the end (right)
+str rtrim(str_cref s)
+{
+    auto it = std::find_if(s.rbegin(), s.rend(), [](char ch)
+    {
+        return !std::isspace(static_cast<unsigned char>(ch));
+    });
+    return str(s.begin(), it.base());
+}
+
+// Trim from both ends
+str trim(str_cref s)
+{
+    return ltrim(rtrim(s));
+}
+
+vector<str> split_string(str_cref line, char sep)
+{
+    vector<str> res {};
+    std::istringstream iss {line};
+
+    str token {};
+
+    while (std::getline(iss, token, sep))
+    {
+        res.push_back(trim(token));
+    }
+
+    return res;
+}
+
+bool contains(str_cref string, str_cref text)
+{
+    return string.find(text) != str::npos;
+}
+
+template<typename T>
+static inline bool is_between(T num, T min, T max)
+{
+    return (num >= min) and (num <= max);
+}
+
+
+// ==============================================
+// ==============================================
+//
+//   _____    ____    _____    ______ 
+//  / ____|  / __ \  |  __ \  |  ____|
+// | |      | |  | | | |  | | | |__   
+// | |      | |  | | | |  | | |  __|  
+// | |____  | |__| | | |__| | | |____ 
+//  \_____|  \____/  |_____/  |______|
+//                                    
+// ==============================================
+// ==============================================
+
+
+u8 hash(str_cref input)
+{
+    u32 value = 0;
+
+    for (char ch : input)
+    {
+        auto ascii_code = static_cast<u32>(ch);
+        value += ascii_code;
+        value *= 17;
+        value %= 256;
+    }
+
+    return static_cast<u8>(value);
+}
+
+u64 part1()
+{
+    auto file_path = "res\\input.txt";
+    auto ifs = std::ifstream(file_path);
+    if (not ifs.is_open())
+        throw std::format("Cannot open file <{}>", file_path);
+
+    str line;
+    std::getline(ifs, line);
+
+    auto steps = split_string(line, ',');
+
+    u64 res = 0;
+    for (const auto& step : steps)
+    {
+        res += hash(step);
+    }
+
+    return res;
+}
+
+u64 part2()
+{
+    auto file_path = "res\\input.txt";
+    auto ifs = std::ifstream(file_path);
+    if (not ifs.is_open())
+        throw std::format("Cannot open file <{}>", file_path);
+
+    for (str line;
+         std::getline(ifs, line);
+         )
+    {
+
+    }
+
+    u64 res = 0;
+    return res;
+}
+
+int main()
+{
+    try
+    {
+        cout << "day 15 part 1: " << part1() << endl;
+        cout << "day 15 part 2: " << part2() << endl;
+    }
+    catch (const char* e)
+    {
+        cout << "[ERROR] " << e << endl;
+    }
+    catch (str_cref e)
+    {
+        cout << "[ERROR] " << e << endl;
+    }
+    catch (const std::exception& e)
+    {
+        cout << "[EXC] " << e.what() << endl;
+    }
+
+    return 0;
+}
