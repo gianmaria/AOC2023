618375140bf7d3d8e9857ae18613767623cc377e
Gianmaria <gianmaria.bonici@gmail.com>
2023-12-29 09:59:48 +0100

    progress...

--- Day13/src/day13.cpp
+++ Day13/src/day13.cpp
@@ -24,6 +24,7 @@
 #include <string_view>
 #include <vector>
 #include <print>
+#include <optional>
 
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -44,6 +45,9 @@ using std::array;
 using std::set;
 using std::map;
 using std::pair;
+using std::print;
+using std::println;
+using std::optional;
 
 using str = std::string;
 using strv = std::string_view;
@@ -158,22 +162,188 @@ static inline bool is_between(T num, T min, T max)
 // ==============================================
 
 
+struct Symmetry_Info
+{
+    u64 pos = 0;
+    u64 len = 0;
+};
+
+optional<Symmetry_Info> check_symmetry(const vec<str>& input)
+{
+    Symmetry_Info si {};
+
+    u64 rows = input.size();
+
+    for (u64 r = 0;
+         r < rows - 1;
+         ++r)
+    {
+        if (input.at(r) != input.at(r + 1))
+            continue;
+
+        si.pos = r;
+
+        // count how "long" the symmetry is
+        for (i64 a = r;
+             a >= 0;
+             --a)
+        {
+            u64 b = r + (r - a) + 1;
+            println("checking {} {}", a, b);
+
+            if (b < rows and
+                input.at(a) == input.at(b))
+            {
+                ++si.len;
+            }
+        }
+
+        return std::make_optional(si);
+    }
+
+    return std::nullopt;
+}
+
+vec<str> transpose(const vec<str>& input)
+{
+    auto t_input = vec<str>(input[0].length(), str(input.size(), 'A'));
+    u64 rows = input.size();
+    u64 cols = input.at(0).size();
+
+    for (u64 r = 0;
+         r < rows;
+         ++r)
+    {
+        for (u64 c = 0;
+             c < cols;
+             ++c)
+        {
+            t_input.at(c).at(r) = input.at(r).at(c);
+        }
+    }
+
+    return t_input;
+}
+
+void print_input(const vec<str>& input)
+{
+    for (const auto& row : input)
+    {
+        println("{}", row);
+    }
+    println("");
+}
+
+pair<bool, u64>
+solve(const vec<str>& input)
+{
+    auto h_symmetry = check_symmetry(input);
+
+    auto t_input = transpose(input);
+    auto v_symmetry = check_symmetry(t_input);
+
+    if (h_symmetry.has_value() and
+        v_symmetry.has_value())
+    {
+        print_input(input);
+        print_input(t_input);
+        println("==========================================");
+
+        if (h_symmetry.value().len > v_symmetry.value().len)
+        {
+            return std::make_pair(true, h_symmetry.value().pos);
+        }
+        else if (h_symmetry.value().len < v_symmetry.value().len)
+        {
+            return std::make_pair(false, v_symmetry.value().pos);
+        }
+        else
+        {
+            if (h_symmetry.value().pos > v_symmetry.value().pos)
+            {
+                return std::make_pair(true, h_symmetry.value().pos);
+            }
+            else if (h_symmetry.value().pos < v_symmetry.value().pos)
+            {
+                return std::make_pair(false, v_symmetry.value().pos);
+            }
+            else
+            {
+                print_input(input);
+                print_input(t_input);
+                println("--------------------------------");
+            }
+        }
+    }
+    else if (h_symmetry.has_value())
+    {
+        //println("Horizontal symmetry");
+        return std::make_pair(true, h_symmetry.value().pos);
+    }
+    else if (v_symmetry.has_value())
+    {
+        //println("Vertical symmetry");
+        return std::make_pair(false, v_symmetry.value().pos);
+    }
+    else
+    {
+        print_input(input);
+        print_input(t_input);
+
+        int s = 0;
+    }
+}
 
 u64 part1()
 {
-    auto file_path = "res\\input.txt";
+    auto file_path = "res\\test.txt";
     auto ifs = std::ifstream(file_path);
     if (not ifs.is_open())
         throw std::format("Cannot open file <{}>", file_path);
+
+    vec<str> puzzle;
+    puzzle.reserve(20);
+    u64 acc = 0;
 
     for (str line;
          std::getline(ifs, line);
          )
     {
-        
+        if (line != "")
+        {
+            puzzle.push_back(std::move(line));
+        }
+        else
+        {
+            auto [h_symmetry, pos] = solve(puzzle);
+            pos += 1;
+            if (h_symmetry)
+            {
+                acc += 100 * pos;
+            }
+            else
+            {
+                acc += pos;
+            }
+
+            puzzle.clear();
+        }
     }
 
-    u64 res = 0;
+    // calculate last one
+    auto [h_symmetry, pos] = solve(puzzle);
+    pos += 1;
+    if (h_symmetry)
+    {
+        acc += 100 * pos;
+    }
+    else
+    {
+        acc += pos;
+    }
+
+
+    u64 res = acc;
     return res;
 }
 
@@ -188,7 +358,7 @@ u64 part2()
          std::getline(ifs, line);
          )
     {
-        
+
     }
 
     u64 res = 0;
@@ -199,9 +369,8 @@ int main()
 {
     try
     {
-        throw "yo";
-        std::println("day 13 part 1: {}", part1());
-        std::println("day 13 part 2: {}", part2());
+        println("day 13 part 1: {}", part1());
+        println("day 13 part 2: {}", part2());
 
         return 0;
     }
@@ -217,6 +386,6 @@ int main()
     {
         cout << "[EXC] " << e.what() << endl;
     }
-    
+
     return 1;
 }
